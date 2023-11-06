cc9b92f820b095bc9da9656c4d741e386cfaa78e
nitiiink1 <nitiiink1@gmail.com>
2023-11-06 21:53:14 +0530

    refactor

--- src/App.js
+++ src/App.js
@@ -1,33 +1,11 @@
-import profilePic from "./public/images/profile.jpg";
 import Blog from "./Blog.js";
-import {useEffect, useState } from "react";
-import axios from 'axios';
+const CDN_PROFILE_PIC = "https://private-user-images.githubusercontent.com/142297462/280767480-5c596464-70cd-4e4e-be90-3f4c339e7e7d.jpg?jwt=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJnaXRodWIuY29tIiwiYXVkIjoicmF3LmdpdGh1YnVzZXJjb250ZW50LmNvbSIsImtleSI6ImtleTEiLCJleHAiOjE2OTkyODY3NDEsIm5iZiI6MTY5OTI4NjQ0MSwicGF0aCI6Ii8xNDIyOTc0NjIvMjgwNzY3NDgwLTVjNTk2NDY0LTcwY2QtNGU0ZS1iZTkwLTNmNGMzMzllN2U3ZC5qcGc_WC1BbXotQWxnb3JpdGhtPUFXUzQtSE1BQy1TSEEyNTYmWC1BbXotQ3JlZGVudGlhbD1BS0lBSVdOSllBWDRDU1ZFSDUzQSUyRjIwMjMxMTA2JTJGdXMtZWFzdC0xJTJGczMlMkZhd3M0X3JlcXVlc3QmWC1BbXotRGF0ZT0yMDIzMTEwNlQxNjAwNDFaJlgtQW16LUV4cGlyZXM9MzAwJlgtQW16LVNpZ25hdHVyZT1lNDdlNGFjMTJmOTAwM2FhMjUzNWRmNzRiMjY1OTViM2ZlOTQ1ZjNjNjEyNjhlNjE0ZGNiODIxMGNhZDcwZTMxJlgtQW16LVNpZ25lZEhlYWRlcnM9aG9zdCZhY3Rvcl9pZD0wJmtleV9pZD0wJnJlcG9faWQ9MCJ9.pWt7eNy-j97L4iO1vjI8anDKtklrx2VEMhqLkf03hP8";
 
 function App() {
-  const [data, setData] = useState("");
-  
-  const getData = async function () {
-    console.log('making the api call');
-    try {
-      await axios.get('/hello').then((res) => {
-        setData(res.data.message);
-      });
-    } catch (error) {
-      console.log(error);
-      setData("error123")
-    }
-
-  }
-  
-  useEffect(()=>{
-    getData()
-  }, [])
-  
   return (
     <div>
-      <img src={profilePic} alt="nitin" />
+      <img src={CDN_PROFILE_PIC} alt="nitin" />
       <h1>Nitin Kumar</h1>
-      <h2>{data}</h2>
       <p> Hello, I'm Nitin. I'm a Software Engineer passionate about building systems that scale. </p>
       <Blog />
     </div>
