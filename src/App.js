84b99b5497b7b2cc1175c588ad999638db0c0ca5
nitiiink1 <nitiiink1@gmail.com>
2023-11-10 12:21:15 +0530

    css changes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import Blog from "./Blog.js";
 import imageMeta from "./assets/image.json";
+import './styles/App.css';
 
 function App() {
   return (
     <div>
-      <h1>Nitin Kumar</h1>
+      <div className="layout_header">
       <img src= {imageMeta.profile_pic.url} alt={imageMeta.profile_pic.alt}/>
-      <p> Hello, I'm Nitin. I'm a Software Engineer passionate about building systems that scale. </p>
-      <Blog />
+      <h1 className="user_name">Nitin Kumar</h1>
+      <p> Hello, I’m <strong>Nitin</strong>. I'm a Software Engineer passionate about  <br/> building systems that scale. You can contact me on <a href="https://twitter.com/nitiiink">Twitter</a>.</p>
+      </div>
+      {/* <Blog /> */}
     </div>
   );
 }
